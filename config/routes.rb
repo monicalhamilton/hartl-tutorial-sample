03f6f83b001114dd9790e226fec3e33f3dc7cc25
Monica Hamilton <monica@betterment.com>
2016-01-24 22:33:41 -0500

    Added basic users controller with new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'static_pages#home'
 
   get 'help' => 'static_pages#help'
@@ -7,4 +9,6 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
 
   get 'contact' => 'static_pages#contact'
+
+  get 'signup' => 'users#new'
 end
